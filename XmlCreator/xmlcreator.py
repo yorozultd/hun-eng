6895642f6c418f4df1eef5ac5b2103e21c3abb72
Ch34TC0D3 <ch34tc0d3@pop-os.localdomain>
2019-09-15 19:09:47 +0530

    running

--- XmlCreator/xmlcreator.py
+++ XmlCreator/xmlcreator.py
@@ -8,17 +8,29 @@ with open("csv_arak_ebrand_tulajs.csv") as f:
     a=csv.reader(f)
     for i in a:
         product = ET.SubElement(products, 'product')
+        field=1;
         for j in i  :
             data= j.split("|")
             for k in data : 
                 if(k!=''):
                     fields = ET.SubElement(product, 'field_'+str(field))
                     if(dictionary.item().get(k.lower())==None):
-                        fields.text =  k;
+                        if(len(k.strip().split(' '))>1 ):
+                            K=k.strip().split(" ")
+                            print(K)
+                            for l in range(len(K)):
+                                if(dictionary.item().get(K[l].lower())==None):
+                                    pass
+                                else: 
+                                    K[l]=dictionary.item().get(K[l].lower())[0]
+                            print(K)                           
+                            fields.text= str(" ".join(K)).strip()
+                        else :
+                            fields.text =  k;
                     else :
                         fields.text =dictionary.item().get(k.lower())[0]
                     field+=1
 mydata = ET.tostring(products)
 myfile = open("../Output/xmlData.xml", "w")
-myfile.write(mydata.decode('utf-8'))
+myfile.write(mydata.decode('windows-1250'))
 myfile.close()
